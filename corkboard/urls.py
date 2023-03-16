8f4b8cf45c58aaf08fa1e6b373fd8f093c69b221
Sartaj Dua <duass@uci.edu>
2023-03-16 12:16:03 -0700

    added past restaurants

--- corkboard/urls.py
+++ corkboard/urls.py
@@ -7,5 +7,7 @@ router = DefaultRouter()
 router.register('users', viewsets.UserViewSet)
 urlpatterns = [
     path('restaurants/latitude/<latitude>/longitude/<longitude>/username/<username>', views.index, name='index'),
+    path('users/<user_id>/restaurants', views.get_past_restaurants, name='past_restaurants'),
+    path('users/<user_id>/restaurants/<restaurant_name>/rating/<rating>/update', views.update_restaurant_rating, name='update_rating'),
     path('', include(router.urls)),
 ]
--- corkboard/views.py
+++ corkboard/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 import googlemaps
 import json
 from datetime import datetime
-from corkboard.models import User
+from corkboard.models import User, Restaurant
 import requests
 
 '''
@@ -11,30 +11,71 @@ User
 1) Access data members of user using "user.<data_member>"
 '''
 
+def update_restaurant_rating(request, user_id, restaurant_name, rating):
+    restaurant = Restaurant.objects.get(user=user_id, name=restaurant_name)
+    restaurant.user_rating = rating
+    restaurant.save()
+    return HttpResponse(json.dumps({'status': 'success'}))
+
+def get_past_restaurants(request, user_id):
+    restaurants = Restaurant.objects.filter(user=user_id)
+    def format_restaurant(r):
+        new_r = dict()
+        new_r['name'] = r.name
+        new_r['user_id'] = r.user_id
+        new_r['user_rating'] = r.user_rating
+        return new_r
+
+    restaurants = list(map(format_restaurant, restaurants))
+    return HttpResponse(json.dumps(restaurants))
+
+
+
 def index(request, latitude, longitude, username):
     gmaps = googlemaps.Client(key='AIzaSyBYm9_ggjcyAUFBLBhr8fV79fvNpXUoqOw')
     nearby_restaurants = gmaps.places_nearby(location=(latitude, longitude), radius=10000, language='english',
                                              type='restaurant')
-    user = User.objects.get(username=username) # need label for food and cuisine
+    user = User.objects.get(username=username)  # need label for food and cuisine
 
     def get_restaurant_details(restaurant):
         r = gmaps.place(restaurant['place_id'])['result']
-        photo = requests.get(f'https://maps.googleapis.com/maps/api/place/photo?maxwidth=300&photo_reference={r["photos"][0]["photo_reference"]}&key=AIzaSyBYm9_ggjcyAUFBLBhr8fV79fvNpXUoqOw')
+        photo = requests.get(
+            f'https://maps.googleapis.com/maps/api/place/photo?maxwidth=300&photo_reference={r["photos"][0]["photo_reference"]}&key=AIzaSyBYm9_ggjcyAUFBLBhr8fV79fvNpXUoqOw')
         r['picture_url'] = photo.url
         return r
+
     restaurant_details = map(get_restaurant_details, nearby_restaurants['results'])
 
-    sorted_restaurants = sorted(restaurant_details, key=lambda r : rate_restaurant(gmaps,r,user), reverse=True)
+    past_restaurants = Restaurant.objects.filter(user=user.id)
+    sorted_restaurants = sorted(restaurant_details, key=lambda r: rate_restaurant(gmaps, r, user, past_restaurants),
+                                reverse=True)
     for r in sorted_restaurants:
-        print(r['name'], rate_restaurant(gmaps,r,user))
+        print(r['name'], rate_restaurant(gmaps,r,user, past_restaurants))
+    top_restaurant = sorted_restaurants[0]
+    past_restaurants = Restaurant.objects.filter(user=user.id, name=top_restaurant['name'].lower())
+    if (len(past_restaurants) == 0):
+        Restaurant.objects.create(name=top_restaurant['name'].lower(),
+                                  google_id=top_restaurant['place_id'],
+                                  user_rating=3,
+                                  user=user)
     return HttpResponse(json.dumps(sorted_restaurants))
 
-def rate_restaurant(gmaps, restaurant, user):
-    food_list = [{'rating':user.first_dish, 'foods':['mexican','taco','burrito','asada','pastor']},
-                 {'rating':user.second_dish, 'foods':['persian', 'greek', 'mediterranean','pita','koobideh', 'kebabs']},
-                 {'rating':user.third_dish, 'foods':['pizza','pepperoni']},
-                 {'rating':user.fourth_dish, 'foods':['salad','healthy']},
-                 {'rating':user.fifth_dish, 'foods':['sandwich','healthy', 'lettuce']},]
+
+def rate_restaurant(gmaps, restaurant, user, past_restaurants):
+    if 'rating' not in restaurant:
+        return 0
+    rating = restaurant['rating']
+    for pr in past_restaurants:
+        if pr.name == restaurant['name'].lower():
+            rating = pr.user_rating
+            break
+
+    food_list = [{'rating': user.first_dish, 'foods': ['mexican', 'taco', 'burrito', 'asada', 'pastor']},
+                 {'rating': user.second_dish,
+                  'foods': ['persian', 'greek', 'mediterranean', 'pita', 'koobideh', 'kebabs']},
+                 {'rating': user.third_dish, 'foods': ['pizza', 'pepperoni']},
+                 {'rating': user.fourth_dish, 'foods': ['salad', 'healthy']},
+                 {'rating': user.fifth_dish, 'foods': ['sandwich', 'healthy', 'lettuce']}, ]
     '''
     api call to gmaps.place(restaurant_id)
     chrome: http://127.0.0.1:8000/corkboard/restaurants/latitude/<latitude>/longitude/<longitude>/username/<username>
@@ -53,16 +94,16 @@ def rate_restaurant(gmaps, restaurant, user):
     for food_dict in food_list:
         for food in food_dict['foods']:
             if food in restaurant['name'].lower():
-                return food_dict['rating'] + restaurant['rating'] - 3
+                return food_dict['rating'] + rating - 3
             if 'editorial_summary' in restaurant:
                 if food in restaurant['editorial_summary']['overview'].lower():
-                    return food_dict['rating'] + restaurant['rating'] - 3
+                    return food_dict['rating'] + rating - 3
                 for r in restaurant['reviews']:
                     if food in r['text'].lower():
-                        return food_dict['rating'] + restaurant['rating'] - 3
+                        return food_dict['rating'] + rating - 3
     if 'rating' not in restaurant:
         return 0
-    return restaurant['rating']
+    return rating
     '''
     print(restaurant['name'])
     print("Rating:", restaurant['rating'])
@@ -71,6 +112,8 @@ def rate_restaurant(gmaps, restaurant, user):
     [print("REVIEW\n" + x['text']) for x in details['result']['reviews']]
     print()
     '''
+
+
 """
 users = User.objects.
 all = gets all users information
